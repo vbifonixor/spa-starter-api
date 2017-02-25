c687491a61d9dad76d431a1283ad84c7e91d3f6a
Lucas Pires <lucas.pires.mattos@gmail.com>
2017-02-25 14:08:43 -0300

    books table seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(AuthorsTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
     }
 }
