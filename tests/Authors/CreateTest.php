528a2edaeb88346b0e6db13ef0f34b79bfefd43c
Lucas Pires <lucas.pires.mattos@gmail.com>
2017-02-25 11:56:56 -0300

    create test

---
+++ tests/Authors/CreateTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateTest extends TestCase
+{
+    use WithoutMiddleware, DatabaseMigrations;
+
+    public function testIsValidatingFields()
+    {
+        $this->json('POST', '/api/authors');
+
+        $this->assertResponseStatus(422)
+            ->seeJsonStructure([
+                'errors' => [[]],
+            ]);
+    }
+
+    public function testCanCreateAuthor()
+    {
+        $this->json('POST', '/api/authors', [
+            'name' => 'Daniel Wallace',
+        ]);
+
+        $this->assertResponseStatus(201)
+            ->seeInDatabase('authors', [
+                'name' => 'Daniel Wallace',
+            ])->seeJson([
+                'name' => 'Daniel Wallace',
+            ])->seeJsonStructure([
+                'data' => ['id', 'name'],
+            ]);
+    }
+}
