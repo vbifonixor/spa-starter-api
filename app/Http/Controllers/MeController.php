c4a547e8de636ad2d0f1616ebd03878a94d02027
Lucas Pires <lucas.pires.mattos@gmail.com>
2017-03-02 14:50:22 -0300

    show authenticated user

---
+++ app/Http/Controllers/MeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+class MeController extends Controller
+{
+    /**
+     * Creates a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('jwt.auth');
+    }
+
+    /**
+     * Show authenticated user.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show()
+    {
+        return response()->json([
+            'data' => Auth::user(),
+        ], 200);
+    }
+}
